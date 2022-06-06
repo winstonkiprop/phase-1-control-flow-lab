035d8c2e638bbeafd1e49a55b6d7c43ba3226482
winston kiprop <winstonekip50@gmail.com>
2022-06-06 21:58:04 +0300

    commit tree 0ea568b6f2772446273b4b2b4865563c4afec9c4

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  if (distance<= 400){
+    return 'This one is on me!';
+  }
+  else if (distance>2000 && distance<2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.';
+  }
+};
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else {
+    return 'No go.';
+  }
+};
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not  generous':
+      return 'Thank you.';
+    default:
+      return "Bye.";
+  }
 }
